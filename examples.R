9497393fe2541f7afd867bc8768cc2f4b4f04bd9
Hilary Parker <hiparker@jhsph.edu>
2012-08-29 16:30:36 -0400

    Updated examples for 2012
    Also removed lab instructor example

--- examples.R
+++ examples.R
@@ -6,28 +6,18 @@ setwd("C:/Users/Hilary/GitHub/GenerateCalendar")
 library(chron)
 source("gen_cal.R")
 
-set.seed(12345)
+set.seed(52085)
 
-gen_cal(wd="C:/Users/Hilary/Desktop",
-		ta_names=c("Sun Eun", "Paige", "Tom", "Rinda", "Elizabeth", "Shu-Yi", "Yenny", "Sherlly", "Jingyan",
-				   "Ah Young", "Johnny", "Katherine",  "Seung Hee", "Juemin", "Yifei"),
-		start_date="01/25/12",
-		end_date="03/14/12",
-		no_TA_dates=c("02/21/12","02/22/12"),
+gen_cal(wd="C:/Users/Hilary/Desktop/cals",
+		ta_names=c("Rawan", "Jiawei", "Leonardo", "Lillie", "Lei", "Huitong", "Haochang",
+				   "Mariana", "David", "Goro", "Sunny", "Jose"),
+		start_date="09/10/12",
+		end_date="10/23/12",
+		no_TA_dates=c("09/24/12"),
 		lab=FALSE,
-		ta_location="TA Office Hour = W2021; STATA Office Hour = W3025",
+		ta_location="",
 		lab_location=c(),
-		double_days=c("02/06/12","02/10/12","02/15/12","02/20/12","02/29/12",
-					  "03/05/12","03/12/12","03/14/12"
+		double_days=c("09/12/12","09/17/12","09/19/12","09/24/12",
+					  "10/08/12","10/10/12","10/22/12","10/24/12"
 					  )
-		)
-
-gen_cal(wd="C:/Users/Hilary/Desktop",
-		ta_names=c("Hilary","Kirsten","Marie"),
-		start_date="09/22/11",
-		end_date="10/1/11",
-		no_TA_dates=c("09/23/11","09/29/11"),
-		ta_location<-"TA Office Hour = W2021; STATA Office Hour = W3025",
-		double_days=c("09/22/11","09/30/11"),
-		lab_location<-"W3031"
 		)
