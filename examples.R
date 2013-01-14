5492315c96ae09b40f86a9fb21e5d779d6f0050b
Hilary Parker <hiparker@jhsph.edu>
2013-01-14 15:44:34 -0500

    new data

--- examples.R
+++ examples.R
@@ -9,15 +9,16 @@ source("gen_cal.R")
 set.seed(52085)
 
 gen_cal(wd="C:/Users/Hilary/Desktop/cals",
-		ta_names=c("Rawan", "Jiawei", "Leonardo", "Lillie", "Lei", "Huitong", "Haochang",
-				   "Mariana", "David", "Goro", "Sunny", "Jose"),
-		start_date="09/10/12",
-		end_date="10/23/12",
-		no_TA_dates=c("09/24/12"),
+		ta_names=c("Jiawei","Leonardo","Lillie","Mariana","David","Goro","Haochang",
+				   "Lei","Rawan","Huitong","Yenny","Juemin","Jose","Sun Eun"
+				   ),
+		start_date="10/30/12",
+		end_date="12/19/12",
+		no_TA_dates=c("11/22/12","11/23/12"),
 		lab=FALSE,
 		ta_location="",
 		lab_location=c(),
-		double_days=c("09/12/12","09/17/12","09/19/12","09/24/12",
-					  "10/08/12","10/10/12","10/22/12","10/24/12"
+		double_days=c("11/05/12","11/07/12","11/14/12","11/19/12",
+					  "12/05/12","12/10/12","12/17/12","12/19/12"
 					  )
 		)
